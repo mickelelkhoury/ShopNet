a65341580193e6d2373e65e6b24d015c827c0afd
Mickel <mickelelkhoury-work@outlook.com>
2021-07-31 00:27:44 +0300

    unhandled rejection error handling

--- server/index.js
+++ server/index.js
@@ -21,8 +21,17 @@ app.use('/api/v1', productRoutes);
 // ERROR MIDDLEWARE
 app.use(errorMiddleware);
 
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
 	console.log(
 		`SERVER LISTENING ON PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`
 	);
+});
+
+// Handle Unhandled promise rejections
+process.on('unhandledRejection', (err) => {
+	console.error(`ERROR: ${err.message}`);
+	console.log('STOPING SERVER DUE TO UNHANDLED PROMISE REJECTION');
+	server.close(() => {
+		process.exit(1);
+	});
 });
