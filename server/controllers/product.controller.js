aade0c5aaa91e4a12d213d0395ba6c6dce9f51af
Mickel <mickelelkhoury-work@outlook.com>
2021-07-31 14:29:26 +0300

    api features: pagination query

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -15,7 +15,13 @@ module.exports.newProduct = catchAsyncErrors(async (req, res, next) => {
 
 // GET ALL PRODUCTS => /api/v1/product
 module.exports.getProducts = catchAsyncErrors(async (req, res, next) => {
-	const apiFeatures = new APIFeatures(Product.find(), req.query).search();
+	const resPerPage = 4;
+	const productCount = await Product.countDocuments();
+
+	const apiFeatures = new APIFeatures(Product.find(), req.query)
+		.search()
+		.filter()
+		.pagination(resPerPage);
 
 	const products = await apiFeatures.query;
 
@@ -23,6 +29,7 @@ module.exports.getProducts = catchAsyncErrors(async (req, res, next) => {
 		success: true,
 		message: 'All product from DB',
 		count: products.length,
+		productCount,
 		products,
 	});
 });
--- server/utils/apiFeatures.js
+++ server/utils/apiFeatures.js
@@ -33,6 +33,14 @@ class APIFeatures {
 		this.query = this.query.find(JSON.parse(queryStr));
 		return this;
 	}
+
+	pagination(resPerPage) {
+		const currentPage = Number(this.queryString.page) || 1;
+		const skip = resPerPage * (currentPage - 1);
+
+		this.query = this.query.limit(resPerPage).skip(skip);
+		return this;
+	}
 }
 
 module.exports = APIFeatures;
