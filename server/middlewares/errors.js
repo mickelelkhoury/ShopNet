792f40982e64ac21c8ad6a1753d07ab4fd6274dd
Mickel <mickelelkhoury-work@outlook.com>
2021-07-31 13:03:07 +0300

    Handling mongoose validation error

--- server/middlewares/errors.js
+++ server/middlewares/errors.js
@@ -22,6 +22,12 @@ module.exports = (err, req, res, next) => {
 			error = new ErrorHandler(message, 400);
 		}
 
+		// Handling mongoose validation error
+		if (err.name == 'ValidationError') {
+			const message = Object.values(err.errors).map((value) => value.message);
+			error = new ErrorHandler(message, 400);
+		}
+
 		res.status(error.statusCode).json({
 			success: false,
 			message: error.message || 'Internal Server Error',
