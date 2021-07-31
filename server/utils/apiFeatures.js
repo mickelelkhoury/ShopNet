ad42b51ec972991fe050d79891d31f2d8bd03a1b
Mickel <mickelelkhoury-work@outlook.com>
2021-07-31 14:17:17 +0300

    api features: filter query

--- server/utils/apiFeatures.js
+++ server/utils/apiFeatures.js
@@ -18,6 +18,21 @@ class APIFeatures {
 		this.query = this.query.find({ ...keyword });
 		return this;
 	}
+
+	filter() {
+		const queryCopy = { ...this.queryString };
+
+		// Removing fields from the query
+		const removeFields = ['keyword', 'limit', 'page'];
+		removeFields.forEach((el) => delete queryCopy[el]);
+
+		// Filter for price, ratings etc
+		let queryStr = JSON.stringify(queryCopy);
+		queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (match) => `$${match}`);
+
+		this.query = this.query.find(JSON.parse(queryStr));
+		return this;
+	}
 }
 
 module.exports = APIFeatures;
