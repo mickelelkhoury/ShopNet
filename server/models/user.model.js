c7d225c8eb9b486fe2a336ffd3d8d0564ef8eb5c
Mickel <mickelelkhoury-work@outlook.com>
2021-07-31 14:42:42 +0300

    created user model

---
+++ server/models/user.model.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = mongoose.Schema({
+	name: {
+		type: String,
+		required: [true, 'Please enter your name'],
+		maxLength: [30, 'Your name cannot be more than 30 characters'],
+	},
+	email: {
+		typeString,
+		required: [true, 'Please enter your email'],
+		unique: true,
+		validate: [validator.isEmail, 'Please enter a valid email address'],
+	},
+	password: {
+		type: String,
+		required: [true, 'Please enter your password'],
+		minLength: [6, 'Your password must be longer than 6 characters'],
+		select: false,
+	},
+	avatar: {
+		public_id: {
+			type: String,
+			required: true,
+		},
+		url: {
+			type: String,
+			required: true,
+		},
+	},
+	role: {
+		type: String,
+		default: 'user',
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now,
+	},
+	resetPasswordToken: String,
+	resetPasswordExpire: Date,
+});
+
+module.exports = mongoose.model('User', userSchema);
