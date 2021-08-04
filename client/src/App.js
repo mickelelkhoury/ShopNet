5bff22d4c1730cea32162a9601a8c419bf057737
Mickel <mickelelkhoury-work@outlook.com>
2021-08-04 22:06:32 +0300

    added header and footer

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 // COMPONENTS
 import Header from './components/layout/header/Header';
+import Footer from './components/layout/footer/Footer';
 
 function App() {
 	return (
 		<div className='App'>
 			<Header />
+			<Footer />
 		</div>
 	);
 }
---
+++ client/src/components/layout/footer/Footer.js
@@ -0,0 +1,15 @@
+import { Fragment } from 'react';
+
+import './Footer.scss';
+
+const Footer = () => {
+	return (
+		<Fragment>
+			<footer className='footer'>
+				<span className='footer__copyright'>&copy; 2021 Ecommerce</span>
+			</footer>
+		</Fragment>
+	);
+};
+
+export default Footer;
