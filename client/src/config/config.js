83aa472f3a1d425ddd16e4b7e9d3c3c14b6105b9
Mickel <mickelelkhoury-work@outlook.com>
2021-08-30 22:47:13 +0300

    api config

---
+++ client/src/config/config.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const baseURL = 'http://localhost:4000/api/v1';
+
+const errorCatch = (error) => {
+	console.log('ERROR API: ', error);
+	return error;
+};
+
+async function getAllProducts() {
+	const url = `${baseURL}/product?page=1`;
+	return await axios
+		.get(url)
+		.then((res) => res)
+		.catch((err) => {
+			return errorCatch(err);
+		});
+}
+
+export { getAllProducts };
