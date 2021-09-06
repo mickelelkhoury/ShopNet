eff7d301887a207aec0e375ff326013b14c92006
Mickel <mickelelkhoury-work@outlook.com>
2021-09-06 22:30:28 +0300

    fixed loading

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-import { Suspense, lazy } from 'react';
 import {
 	BrowserRouter as Router,
 	Route,
@@ -9,27 +8,24 @@ import {
 // COMPONENTS
 import Header from './components/layout/header/Header';
 import Footer from './components/layout/footer/Footer';
-import Loading from './components/loading/Loading';
 
 // PAGES
-const Home = lazy(() => import('./pages/home/Home'));
+import Home from './pages/home/Home';
 
 function App() {
 	return (
-		<Suspense fallback={<Loading />}>
-			<Router>
-				<div className='App'>
-					<Header />
-					<div className='container'>
-						<Switch>
-							<Route exact path='/' render={() => <Redirect to='/home' />} />
-							<Route exact path='/home' component={Home} />
-						</Switch>
-					</div>
-					<Footer />
+		<Router>
+			<div className='App'>
+				<Header />
+				<div className='container'>
+					<Switch>
+						<Route exact path='/' render={() => <Redirect to='/home' />} />
+						<Route exact path='/home' component={Home} />
+					</Switch>
 				</div>
-			</Router>
-		</Suspense>
+				<Footer />
+			</div>
+		</Router>
 	);
 }
 
--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -8,43 +8,45 @@ import productActions from '../../redux/actions/productActions';
 import MainTextHeader from '../../components/headers/MainTextHeader';
 import ItemCard from '../../components/itemCard/ItemCard';
 import MetaData from '../../components/layout/MetaData';
+import Loading from '../../components/loading/Loading';
 
-const Home = ({
-	getAllProducts,
-	allProductsData,
-	allProductsLoading,
-	allProductsMessage,
-}) => {
+const Home = ({ getAllProducts, allProductsData, allProductsLoading }) => {
 	useEffect(() => {
 		getAllProducts();
 	}, []);
 
 	return (
 		<>
-			<MetaData title='Buy Best Product Online' />
-			<MainTextHeader title='latest products' />
-			<section className='container py-6 px-6-mobile px-3'>
-				<div className='columns is-flex is-flex-wrap-wrap'>
-					{allProductsData?.products &&
-						allProductsData?.products?.map((zone, i) => {
-							return (
-								<div
-									key={i}
-									className='column is-half-mobile is-one-quarter-tablet'
-								>
-									<ItemCard
-										id={zone._id}
-										title={zone.name}
-										images={zone.images}
-										numOfReviews={zone.numOfReviews}
-										ratings={zone.ratings}
-										price={zone.price}
-									/>
-								</div>
-							);
-						})}
-				</div>
-			</section>
+			{allProductsLoading ? (
+				<Loading />
+			) : (
+				<>
+					<MetaData title='Buy Best Product Online' />
+					<MainTextHeader title='latest products' />
+					<section className='container py-6 px-6-mobile px-3'>
+						<div className='columns is-flex is-flex-wrap-wrap'>
+							{allProductsData?.products &&
+								allProductsData?.products?.map((zone, i) => {
+									return (
+										<div
+											key={i}
+											className='column is-half-mobile is-one-quarter-tablet'
+										>
+											<ItemCard
+												id={zone._id}
+												title={zone.name}
+												images={zone.images}
+												numOfReviews={zone.numOfReviews}
+												ratings={zone.ratings}
+												price={zone.price}
+											/>
+										</div>
+									);
+								})}
+						</div>
+					</section>
+				</>
+			)}
 		</>
 	);
 };
