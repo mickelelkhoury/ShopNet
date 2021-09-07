1cff7629a3b63e3a3edec0b5a0e0518bf5bcc4ff
Mickel <mickelelkhoury-work@outlook.com>
2021-09-07 14:59:33 +0300

    main button component

--- client/src/components/button/MainButton.js
+++ client/src/components/button/MainButton.js
@@ -1,13 +1,30 @@
 import React from 'react';
 
-const MainButton = ({ text, color, handleClick, btnWidth, btnHeight }) => {
+const MainButton = ({
+	text,
+	color,
+	handleClick,
+	btnWidth,
+	btnHeight,
+	btnMargin,
+	btnFontSize,
+	icon,
+	...props
+}) => {
 	return (
 		<button
+			{...props}
 			className={`button ${color}`}
 			onClick={handleClick}
-			style={{ width: btnWidth || '100%', height: btnHeight || '100%' }}
+			style={{
+				width: btnWidth || '100%',
+				height: btnHeight || '100%',
+				margin: btnMargin || '0',
+				fontSize: btnFontSize || '16px',
+			}}
 		>
 			{text}
+			<span className='icon'>{icon}</span>
 		</button>
 	);
 };
