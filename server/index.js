5c2a73ca36b6a4c52f5d4e451fdee90283e86bab
Mickel <mickelelkhoury-work@outlook.com>
2021-12-08 11:07:53 +0200

    setup cloudinary

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express');
 const app = express();
 const cookieParser = require('cookie-parser');
+const bodyparser = require('body-parser');
+const cloudinary = require('cloudinary');
 const cors = require('cors');
 
 const connectDB = require('./config/database');
@@ -22,8 +24,16 @@ connectDB();
 
 // MIDDLEWARE
 app.use(express.json());
+app.use(bodyparser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors());
+
+// CLOUDINARY SETUP
+cloudinary.config({
+	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+	api_key: process.env.CLOUDINARY_API_KEY,
+	api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 // ROUTES
 const productRoutes = require('./routes/product.routes');
